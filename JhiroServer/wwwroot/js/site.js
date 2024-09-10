9ec436bc3db3a06cafcbcd013cca0b793e659ed9
Alen Keneddy Aguilar Jara <alenaguilar24@gmail.com>
2024-09-10 17:28:15 -0500

    Adiccion de Stilos y JS para animaciones

---
+++ JhiroServer/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿window.setImageModalSrc = (imageUrl) => {
+    document.getElementById('modalImage').src = imageUrl;
+}
+
+window.showImageModal = () => {
+    document.getElementById('imageModal').style.display = 'block';
+}
+
+window.hideImageModal = () => {
+    document.getElementById('imageModal').style.display = 'none';
+}
