3cfe8179def26b80f8678aa757d5a022e17e7cfe
Alen Keneddy Aguilar Jara <alenaguilar24@gmail.com>
2024-09-18 07:49:46 -0500

    Implementacion de los Servicios de JWT

---
+++ JhiroServer/Services/IJwtService.cs
@@ -0,0 +1,13 @@
+﻿//using JhiroServer.Models;
+//using System.Security.Claims;
+
+//namespace JhiroServer.Services
+//{
+//    public interface IJwtService
+//    {
+//        string GenerateToken(Usuario usuario); 
+//        string GetUserIdFromToken(string token);
+//    }
+
+
+//}
