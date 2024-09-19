f42da627aaa4bd772ab44da11bd2ea9212be1c7c
Alen Keneddy Aguilar Jara <alenaguilar24@gmail.com>
2024-09-18 19:30:01 -0500

    Implementacion del Login

---
+++ JhiroServer/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JhiroServer.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "El correo electrónico es requerido.")]
+        [EmailAddress(ErrorMessage = "Correo electrónico inválido.")]
+        public string Correo { get; set; } = string.Empty;
+
+        
+    }
+}
---
+++ JhiroServer/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace JhiroServer.Models
+{
+    public class LoginRequest
+    {
+        public string Correo { get; set; }
+        
+    }
+}
